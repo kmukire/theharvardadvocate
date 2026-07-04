32f6a3a44231d02dd9dc2061a129fce28a5d22af
Kayleen Mukire <kayleenmukire@college.harvard.edu>
2026-07-03 21:54:52 -0400

    Fix submit page Frame import

--- web/pages/submit.js
+++ web/pages/submit.js
@@ -1,7 +1,7 @@
 /** @jsxImportSource theme-ui */
 import React from "react";
 import Head from "next/head";
-import Frame from "../components/Frame";
+import Frame from "../src/components/Frame";
 
 const submitSx = {
   ".submitBody": {
